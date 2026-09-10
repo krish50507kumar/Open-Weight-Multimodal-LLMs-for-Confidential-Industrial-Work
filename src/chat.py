d9b80dd5d05c61fa7ea75071f097025669ad792e
krish50507kumar <krish50507@gmail.com>
2026-09-10 21:30:49 +0530

    chore: update gitignore to ignore data and pycache

--- src/chat.py
+++ src/chat.py
@@ -1,9 +1,11 @@
 import ollama
-class LLM:
-    def __init__(self,model):
-        self.model = model
-    def chat(self,prompt):
 
+class LLM:
+    def __init__(self, model):
+        self.model = model
+
+    def chat(self, prompt):
+        messages = [{"role": "user", "content": prompt}]
         response = ollama.chat(model=self.model, messages=messages)
         reply = response['message']['content']
         return reply
